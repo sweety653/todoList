2e710f82f053c8ec4172f48b567c776ca9624afe
sweety653 <sweetyvip4@gmail.com>
2026-05-17 19:57:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-console.log('JS Загружен');
-
 const form = document.getElementById('addForm');
 const input = document.getElementById('taskInput');
 const list = document.getElementById('todoList');
+const count = document.getElementById('counter');
+const comp = document.getElementById('BtnComp');
+
 
 let tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
 
-function renderTask(task) {
+let draggedTask = null;
+
+function renderTasks(task) {
   const li = document.createElement('li');
 
   const checkbox = document.createElement('input');
@@ -18,22 +21,64 @@ function renderTask(task) {
   const span = document.createElement('span');
   span.textContent = task.text;
 
+  span.addEventListener('dblclick', function() {
+  const editInput = document.createElement('input');
+  editInput.value = task.text;
+  li.replaceChild(editInput, span);
+  editInput.focus();
+
+  editInput.addEventListener('blur', function() {
+    task.text = editInput.value;
+    span.textContent = task.text;
+    li.replaceChild(span, editInput);
+    saveTasks();
+  });
+});
+  
+  li.draggable = true;
+
+  li.addEventListener('dragstart', function(e) {
+    draggedTask = task;
+    li.classList.add('dragging');
+  });
+
+  li.addEventListener('dragover', function(e) {
+    e.preventDefault();
+});
+
+  li.addEventListener('drop', function(){
+    const fromIndex = tasks.indexOf(draggedTask);
+    const toIndex = tasks.indexOf(task);
+    tasks.splice(fromIndex, 1);
+    tasks.splice(toIndex, 0, draggedTask);
+    saveTasks();
+    renderAll();
+});
+
+
+
+
+
   const delBtn = document.createElement('button');
   delBtn.textContent = '✕';
   delBtn.className = 'delete-btn';
 
+  
   checkbox.addEventListener('change', function() {
     task.done = checkbox.checked;
     li.classList.toggle('completed');
     saveTasks();
+    updateCounter();
   });
 
   delBtn.addEventListener('click', function() {
     tasks = tasks.filter(t => t !== task);
     li.remove();
     saveTasks();
+    updateCounter();
   });
 
+  
   li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(delBtn);
@@ -52,10 +97,46 @@ form.addEventListener('submit', function(event) {
 
   const task = { text: text, done: false };
   tasks.push(task);
-  renderTask(task);
+  renderAll();
   saveTasks();
-
+  updateCounter();
   input.value = '';
 });
 
-tasks.forEach(task => renderTask(task));
+function updateCounter() {
+  const remainingTasks = tasks.filter(t => !t.done).length;
+  count.textContent = `Осталось ${remainingTasks} задач`
+}
+
+comp.addEventListener('click', function() {
+  const allLi = list.querySelectorAll('li');
+  allLi.forEach(li => {
+    if (li.classList.contains('completed')) {
+      li.remove();
+    }
+  });
+  tasks = tasks.filter(task => !task.done);
+  saveTasks();
+  updateCounter();
+});
+
+let currentFilter = 'all';
+
+function renderAll() {
+  list.innerHTML = '';
+  tasks
+    .filter(t => currentFilter === 'all' || (currentFilter === 'done' ? t.done : !t.done))
+    .forEach(task => renderTasks(task));
+}
+
+
+document.getElementById('filters').addEventListener('click', function(e) {
+  if (!e.target.classList.contains('filter-btn')) return;
+  currentFilter= e.target.dataset.filter;
+  document.querySelectorAll('.filter-btn').forEach(b => b.classList.remove('active'));
+  e.target.classList.add('active');
+  renderAll();
+});
+
+renderAll();
+updateCounter();
